4930088d1112c5dd78d2332f291804c779d87cb9
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-01-30 20:20:27 +0100

    feat: Add User and SavedRecipe models

---
+++ LadleMeThis/ LadleMeThis/Models/UserModels/User.cs
@@ -0,0 +1,19 @@
+﻿using LadleMeThis.Models.RecipeModels;
+
+namespace LadleMeThis.Models.UserModels
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        // Navigációk
+        public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+        public ICollection<SavedRecipe> SavedRecipes { get; set; } = new List<SavedRecipe>();
+    }
+
+}
