ffab4b0fa7624dcbecbcc5f9d07f636b7599619c
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-01-31 17:51:58 +0100

    refactor(user-repository): updated interface and improved error handling

--- LadleMeThis/ LadleMeThis/Repositories/UserRepository/IUserRepository.cs
+++ LadleMeThis/ LadleMeThis/Repositories/UserRepository/IUserRepository.cs
@@ -5,9 +5,9 @@ namespace LadleMeThis.Repositories.UserRepository;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllUsersAsync();
-    Task<User> GetUserByIdAsync(int id);
+    Task<User?> GetUserByIdAsync(int id);
     Task AddUserAsync(User user);
     Task UpdateUserAsync(User user);
-    Task<bool> DeleteUserAsync(int id);
+    Task DeleteUserAsync(int id);
 
 }
