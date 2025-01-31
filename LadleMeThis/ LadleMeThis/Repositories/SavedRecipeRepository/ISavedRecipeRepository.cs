895f36b137a0c962875cfc41d482ddb3d4296ccd
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-01-31 17:51:33 +0100

    refactor(saved-recipe-repository): improved error handling and repository methods

--- LadleMeThis/ LadleMeThis/Repositories/SavedRecipeRepository/ISavedRecipeRepository.cs
+++ LadleMeThis/ LadleMeThis/Repositories/SavedRecipeRepository/ISavedRecipeRepository.cs
@@ -5,9 +5,8 @@ namespace LadleMeThis.Repositories.SavedRecipeRepository
 {
     public interface ISavedRecipeRepository
     {
-        Task<SavedRecipe?> GetSavedRecipeAsync(int userId, int recipeId);
         Task<List<SavedRecipe>> GetUserSavedRecipesAsync(int userId);
         Task AddSavedRecipeAsync(SavedRecipe savedRecipe);
-        Task DeleteSavedRecipeAsync(SavedRecipe savedRecipe);
+        Task DeleteSavedRecipeAsync(int userId, int recipeId);
     }
 }
