2588ac4d269e1b35b599aa391e5a4344f6983f63
Bálint <szelasszie@gmail.com>
2025-03-22 10:45:31 +0100

    added various contracts/data transfer objects for authentication/registration

---
+++ LadleMeThis/ LadleMeThis/Models/AuthContracts/RegistrationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LadleMeThis.Models.AuthContracts
+{
+    public record RegistrationRequest(
+        [Required] string FirstName,
+        [Required] string LastName,
+        [Required] string Email,
+        [Required] string Username,
+        [Required] string Password);
+}
