fb151cf16e26321fdfb7d2f730558ae147ce5690
Bálint <szelasszie@gmail.com>
2025-03-24 18:47:44 +0100

    changed the recipe card component's style/appearance

--- frontend/src/components/recipeCard/RecipeCard.jsx
+++ frontend/src/components/recipeCard/RecipeCard.jsx
@@ -1,22 +1,33 @@
-import "./../../../styles/recipeCard.scss";
+import { FaClock, FaTags, FaUtensils, FaStar } from "react-icons/fa";
+import { BiCategory } from "react-icons/bi";
 
 export default function RecipeCard({ recipe }) {
     return (
         <div className="recipe-card">
             <a href="/specificRecipe" className="clickable-recipe-card">
-            <div className="img-container">
-                <img src="/bacon2.jpg" alt="Picture of the current food" />
-            </div>
-            <div className="recipe-name">
-                <h4>{recipe.Name}</h4>
-            </div>
-            <div className="extra-info">
-                Preparation time: {recipe.FullTime} mins <br />
-                Serving size : {recipe.ServingSize} ppl <br />
-                Rating : {recipe.Rating} / 5 <br />
-                Tags : {recipe.Tags.join(", ")} <br /> 
-                Categories : {recipe.Categories.join(", ")}
-            </div>
+                <div className="img-container">
+                    <img src="/bacon2.jpg" alt="Picture of the current food" />
+                </div>
+                <div className="recipe-name">
+                    <h4>{recipe.Name}</h4>
+                </div>
+                <div className="extra-info">
+                    <span>
+                    <FaClock /> {recipe.FullTime} min prep  <br />
+                    </span>
+                    <span>
+                    <FaUtensils /> Serves {recipe.ServingSize} <br />
+                    </span>
+                    <span>
+                    <FaStar /> {recipe.Rating} / 5 <br />
+                    </span>
+                    <span>
+                    <FaTags /> {recipe.Tags.join(", ")} <br />
+                    </span>
+                    <span>
+                    <BiCategory /> {recipe.Categories.join(", ")}
+                    </span>
+                </div>
             </a>
         </div>
     )
