8d034f3d801d9d5a000aff13cf9a6e0494a55ce2
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-25 23:25:18 +0100

    refactor

--- frontend/components/recipe/RecipeCategoryTags.jsx
+++ frontend/components/recipe/RecipeCategoryTags.jsx
@@ -4,15 +4,15 @@ const RecipeCategoryTags = ({ categories, tags }) => (
   <div className="recipe-categories-tags">
     <div className="recipe-categories">
       {categories.map((category) => (
-        <span className="recipe-category" key={category.CategoryId}>
-          {category.Name}
+        <span className="recipe-category" key={category.categoryId}>
+          {category.name}
         </span>
       ))}
     </div>
     <div className="recipe-tags">
       {tags.map((tag) => (
-        <span className="recipe-tag" key={tag.TagId}>
-          <FaTags className="recipe-icon" /> {tag.Name}
+        <span className="recipe-tag" key={tag.tagId}>
+          <FaTags className="recipe-icon" /> {tag.name}
         </span>
       ))}
     </div>
