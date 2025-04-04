307f4741dd1e50462fd0a657a88587e1746e755b
Bálint <szelasszie@gmail.com>
2025-04-04 15:44:19 +0200

    basic setup for external random images

--- frontend/src/components/recipeCard/RecipeCard.jsx
+++ frontend/src/components/recipeCard/RecipeCard.jsx
@@ -2,8 +2,10 @@
 import { FaClock, FaTags, FaUtensils, FaStar } from "react-icons/fa";
 import { BiCategory } from "react-icons/bi";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function RecipeCard({ recipe, navigateTo }) {
+    const [imageUrl, setImageUrl] = useState(null);
     const router = useRouter();
 
 
@@ -16,11 +18,29 @@ export default function RecipeCard({ recipe, navigateTo }) {
         }
     }
 
+    useEffect(() => {
+        fetch("https://api.pexels.com/v1/search?query=food&per_page=1", {
+            headers: {
+                Authorization: apiKey
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.photos && data.photos.length > 0) {
+                setImageUrl(data.photos[0].src.original);
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching food image:", error);
+            setImageUrl("/bacon2.jpg");
+        });
+    }, []);
+
 
     return (
         <div onClick={() => handleClick()} className="recipe-card">
             <div className="img-container">
-                <img src="/bacon2.jpg" alt="Picture of the current food" />
+                <img src={imageUrl} alt="Picture of the current food" />
             </div>
             <div className="recipe-name">
                 <h4>{recipe.name}</h4>
