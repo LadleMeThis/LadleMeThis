503a8d2d804e5abc2818d3364e3e69772c874d31
Bálint <szelasszie@gmail.com>
2025-04-04 13:57:30 +0200

    small fix on the footer's display

--- frontend/components/footer/Footer.jsx
+++ frontend/components/footer/Footer.jsx
@@ -5,12 +5,9 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <div className="footer-links">
-                    <a href="#home">Home</a>
-                </div>
                 <div className="footer-copy">
                     <p>
-                        &copy; 2025 Your Company. All Rights Reserved.{' '}
+                        &copy; 2025 Ladle Me This. All Rights Reserved.{' '}
                         <a href="#privacy-policy">Privacy Policy</a>
                     </p>
                 </div>
