1a469e008dd1319f90249b9e534278cc85ccf618
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-04-06 17:11:42 +0200

    Added create rating to recipe page

--- frontend/app/recipe/[recipeId]/[recipeName]/page.jsx
+++ frontend/app/recipe/[recipeId]/[recipeName]/page.jsx
@@ -8,16 +8,91 @@ import RecipeIngredients from "@/components/recipe/RecipeIngredients";
 import RecipeRatings from "@/components/recipe/RecipeRatings";
 import { fetchRecipeById } from "@/scripts/scripts";
 import Loader from "@/components/loader/Loader";
+import CreateRecipeRating from "@/components/recipe/CreateRecipeRating";
+import { fetchCreateRating } from "@/scripts/scripts";
+import { useToast } from "@/context/ToastContext";
 
 const Recipe = () => {
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(true);
   const { recipeId } = useParams();
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [formData, setFormData] = useState({
+    rating: 0,
+    review: ""
+  })
+  const [ratings, setRatings] = useState([])
+  const { showToast } = useToast();
+
+
+  const checkAuthentication = async () => {
+    try {
+      const response = await fetch("/api/check-auth");
+      const data = await response.json();
+
+      setIsAuthenticated(data);
+
+    } catch (err) {
+      console.error(err);
+
+      setIsAuthenticated(false);
+    }
+  };
+  
+  const clearFormdata = () => setFormData({
+    rating: 0,
+    review: ""
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const ratingId = await fetchCreateRating(recipeId, formData)
+
+      const updatedRatings = [...ratings]
+
+      updatedRatings.push({
+        ratingId: ratingId,
+        user:{
+          userName: null
+        },
+        rating: formData.rating,
+        review: formData.review,
+        dateCreated: new Date().toISOString()
+      })
+
+      setRatings(updatedRatings)
+      clearFormdata()
+      
+      showToast("Successfully added rating")
+    } catch (error) {
+      showToast(error.message)
+    }
+
+  }
+
+  const handleRatingChange = (changedRating) => {
+    setFormData(current => ({
+      ...current,
+      rating: changedRating
+    }))
+  }
+
+  const handleReviewChange = (changedReview) => {
+    setFormData(current => ({
+      ...current,
+      review: changedReview
+    }))
+  }
+
+  const isDisabled = () => !isAuthenticated || formData.rating === 0 || formData.review === "";
 
   useEffect(() => {
     const handleRecipe = async () => {
       const data = await fetchRecipeById(recipeId)
       setRecipe(data)
+      setRatings(data.ratings)
+      console.log(data.ratings[0]);
 
       setTimeout(() => {
         setLoading(false);
@@ -26,17 +101,23 @@ const Recipe = () => {
     };
 
     handleRecipe()
+    checkAuthentication()
 
     return () => {
       setRecipe(null);
-      setLoading(true); 
+      setLoading(true);
     };
 
   }, [recipeId])
 
+  useEffect(() => {
+      console.log(isDisabled());
+    
+  }, [isAuthenticated, formData]);
+
   if (loading)
     return <Loader />
-  
+
 
   return (
     <div className="recipe wrapper">
@@ -53,7 +134,14 @@ const Recipe = () => {
       <RecipeCategoryTags categories={recipe.categories} tags={recipe.tags} />
       <p className="recipe-instructions">{recipe.instructions}</p>
       <RecipeIngredients ingredients={recipe.ingredients} />
-      <RecipeRatings ratings={recipe.ratings} />
+      <RecipeRatings ratings={ratings} />
+      {isAuthenticated && <CreateRecipeRating
+        rating={formData.rating}
+        handleRatingChange={handleRatingChange}
+        review={formData.review}
+        handleReviewChange={handleReviewChange}
+        handleSubmit={handleSubmit}
+        isDisabled={isDisabled} />}
     </div>
   );
 };
