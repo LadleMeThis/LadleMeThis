95733a7f94f54af30080b7abf23cc0edfd352345
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-04-28 11:26:52 +0200

    added image proxy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://localhost:8080/:path*', 
-          },
-        ];
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/:path*', 
       },
-    };
-    
+    ];
+  },
+  images: {
+    domains: ['images.pexels.com'],
+  },
+};
 
 export default nextConfig;
