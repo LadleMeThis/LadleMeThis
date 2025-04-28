9d5cdcef403f0d7ab64e4f0df555723c4a728e95
Bálint <szelasszie@gmail.com>
2025-04-28 17:32:51 +0200

    added UserLogger helper class

---
+++ backend/LadleMethisIntegrationTests/UserLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadleMethisIntegrationTests
+{
+    public class UserLogger
+    {
+        private readonly HttpClient _client;
+
+        public UserLogger(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<HttpResponseMessage> LoginUser(string email, string password)
+        {
+            var loginRequest = new
+            {
+                Email = email,
+                Password = password
+            };
+
+            return await _client.PostAsJsonAsync("/Auth/Login", loginRequest);
+        }
+
+        public void AttachAuthCookies(HttpResponseMessage loginResponse)
+        {
+            var cookies = loginResponse.Headers.GetValues("Set-Cookie").ToList();
+            foreach (var cookie in cookies)
+            {
+                _client.DefaultRequestHeaders.Add("Cookie", cookie);
+            }
+        }
+    }
+}
