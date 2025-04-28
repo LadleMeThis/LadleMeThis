18012f93341f81c3119ec82eef2bba21121542ae
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-04-28 11:25:51 +0200

    deleted unused model and it's servecies

--- backend/LadleMeThis/Models/SavedRecipesModels/SavedRecipeResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LadleMeThis.Models.SavedRecipesModels
-{
-    public class SavedRecipeResponseDTO
-    {
-        public string? RecipeName { get; set; }
-        public DateTime DateSaved { get; set; }
-    }
-}
