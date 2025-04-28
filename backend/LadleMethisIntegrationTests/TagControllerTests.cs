a89742694bd20fcbd63ddc1272de19757ddd1699
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-04-28 16:42:13 +0200

    Added tag controller tests

--- backend/LadleMethisIntegrationTests/TagControllerTests.cs
+++ backend/LadleMethisIntegrationTests/TagControllerTests.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using LadleMeThis.Models.ErrorMessages;
 using LadleMeThis.Models.TagModels;
 
 namespace LadleMeThisIntegrationTests;
@@ -19,7 +21,7 @@ public class TagControllerTests(LadleMeThisFactory factory) : IClassFixture<Ladl
         var response = await _client.PostAsJsonAsync("/tags", newTag);
 
         // Assert
-        response.EnsureSuccessStatusCode(); // Status code 200-299
+        response.EnsureSuccessStatusCode(); 
         var createdTag = await response.Content.ReadFromJsonAsync<TagDTO>();
         Assert.NotNull(createdTag);
         Assert.Equal("TestTag", createdTag.Name);
@@ -58,5 +60,47 @@ public class TagControllerTests(LadleMeThisFactory factory) : IClassFixture<Ladl
         // Assert
         Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
         Assert.Equal(HttpStatusCode.NotFound, getResponse.StatusCode);
+    }
+    
+    [Fact]
+    public async Task DeleteByIdAsync_Fails()
+    {
+        // Arrange
+        const int tagId = -1;
+        
+        // Act
+        var deleteResponse = await _client.DeleteAsync($"/tag/{tagId}");
+        var responseContent = await deleteResponse.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, deleteResponse.StatusCode);
+        Assert.Equal(ErrorMessages.NotFoundMessage, responseContent);
+    }
+    
+    [Fact]
+    public async Task AddAsync_Fails()
+    {
+        // Arrange
+        var newTag = new TagCreateRequest {  };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/tags", newTag);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.Contains("validation error", responseContent, StringComparison.OrdinalIgnoreCase);
+    }
+    
+    [Fact]
+    public async Task GetAllAsync_ReturnsAllTags()
+    {
+        // Act
+        var response = await _client.GetAsync("/tags");
+        var responseContent = await response.Content.ReadFromJsonAsync<List<TagDTO>>();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsType<List<TagDTO>>(responseContent);
     }
 }
